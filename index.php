9c96a58f18390f53d4cfb0b9bbcbf0e958939a76
kea-daron <daron3327@gmail.com>
2025-05-29 11:21:15 +0700

    ..

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $categories->execute();
 $category = $categories->fetchAll(PDO::FETCH_OBJ);
 ?>
 
-<html lang="en">
+
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
